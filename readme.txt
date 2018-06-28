32fccf6716b3b40fa87ce296331e42d4bf6b5c7e
Ashish Shrestha <ashishsthanp@gmail.com>
2018-06-28 16:06:41 +0545

    Version update to 1.0.2

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: ThemeGrill
 Tags: theme, suffice, toolkit, themegrill
 Requires at least: 4.0
 Tested up to: 4.8
-Stable tag: 1.0.1
+Stable tag: 1.0.2
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -31,7 +31,7 @@ Get free support at https://themegrill.com/support-forum/
 * The plugin currently only supports Suffice theme.
 
 == Changelog ==
-= TBD =
+= 1.0.2 - 2018-06-28 =
 * Update - Heading tags in widgets.
 
 = 1.0.1 - 2018-06-08 =
--- suffice-toolkit.php
+++ suffice-toolkit.php
@@ -3,7 +3,7 @@
  * Plugin Name: Suffice Toolkit
  * Plugin URI: https://themegrill.com/themes/suffice
  * Description: Suffice Toolkit is a companion for Suffice WordPress theme by ThemeGrill
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: ThemeGrill
  * Author URI: http://themegrill.com
  * License: GPLv3 or later
@@ -29,7 +29,7 @@ final class SufficeToolkit {
 	 * Plugin version.
 	 * @var string
 	 */
-	public $version = '1.0.1';
+	public $version = '1.0.2';
 
 	/**
 	 * Instance of this class.
