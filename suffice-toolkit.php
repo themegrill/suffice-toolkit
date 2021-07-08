e48ff673832768a8fd90a33d33dbc8eac9f73caa
Premiii <jimeepremi@gmail.com>
2021-07-08 21:03:25 +0545

    Version Updated to 1.0.6

--- suffice-toolkit.php
+++ suffice-toolkit.php
@@ -3,7 +3,7 @@
  * Plugin Name: Suffice Toolkit
  * Plugin URI: https://themegrill.com/themes/suffice
  * Description: Suffice Toolkit is a companion for Suffice WordPress theme by ThemeGrill
- * Version: 1.0.5
+ * Version: 1.0.6
  * Author: ThemeGrill
  * Author URI: http://themegrill.com
  * License: GPLv3 or later
@@ -29,7 +29,7 @@ final class SufficeToolkit {
 	 * Plugin version.
 	 * @var string
 	 */
-	public $version = '1.0.5';
+	public $version = '1.0.6';
 
 	/**
 	 * Instance of this class.
